eb65390dc59aed4156dda0d3bfd0c56082a4afa6
Étienne Bouchard <etiennebouchard7@gmail.com>
2020-03-13 08:23:19 -0400

    Changement de nom

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(FlutterLicenseCheck());
 
-class MyApp extends StatelessWidget {
+class FlutterLicenseCheck extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LicenseCheck(title: 'FlutterCineticLicenseCheck'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class LicenseCheck extends StatefulWidget {
+  LicenseCheck({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -40,10 +31,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LicenseCheckState createState() => _LicenseCheckState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _LicenseCheckState extends State<LicenseCheck> {
   int _counter = 0;
 
   void _incrementCounter() {
